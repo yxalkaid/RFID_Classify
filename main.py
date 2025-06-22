59210527a2f129ab4271d1d3234462445e4777ee
糖糖 <yx_alkaid@qq.com>
2025-06-22 12:06:14 +0800

    feat: 初始化项目结构并添加 Hyrda 配置
    - 更新 .gitignore 文件，添加新的忽略目录
    - 新增配置文件和模型配置，为项目设置默认配置
    - 重构 main.py，使用 Hyrda 初始化应用
    - 更新 requirements.txt，添加新依赖项

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+import hydra
+from omegaconf import DictConfig
+import warnings
+import logging
+
+
+@hydra.main(config_path="./config", config_name="config", version_base=None)
+def main(cfg: DictConfig):
+    """
+    启动函数
+    """
+    logging.info("Application Launch")
+
+    warnings.warn("The startup program has not been implemented yet")
+
+
 if __name__ == "__main__":
-    raise NotImplementedError("main.py is not implemented")
+    main()
